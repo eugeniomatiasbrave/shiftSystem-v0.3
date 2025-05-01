6621c9829dff59f1558c69c437fbe632622472ca
eugeniomatiasbrave <107762123+eugeniomatiasbrave@users.noreply.github.com>
2025-05-01 19:03:23 -0300

    shiftRouter

--- server-v0.3/src/router/ShiftsRouter.js
+++ server-v0.3/src/router/ShiftsRouter.js
@@ -5,15 +5,20 @@ import {passportCall} from "../middlewares/passportCall.js";
 class ShiftsRouter extends BaseRouter {
 	
 	init(){
-		this.get('/',['PUBLIC','USER'], passportCall('current'),shiftController.getShifts); /* Consultar todos los turnos */
-        this.put('/reserve/:id_shift', ['PUBLIC','USER'],passportCall('current'), shiftController.reserveShift); /* Reservar un turno */
+        // /api/shifts); Consultar todos los turnos ..................................................................OK
+		this.get('/',['PUBLIC','USER'], passportCall('current'),shiftController.getShifts); 
+        // /api/shifts/user/${id_user}; Consultar turnos por usuario .................................................OK
+        this.get('/user/:id_user',['PUBLIC','USER'], passportCall('current'),shiftController.getShiftsByUser); 
+        // /api/shifts/reserve/:id_shift; Reservar un turno ..........................................................OK
+        this.put('/reserve/:id_shift', ['PUBLIC','USER'],passportCall('current'), shiftController.reserveShift); 
+        // /api/shifts/cancel/:id_shift; Cancelar un turno ...........................................................
+        this.put('/cancel/:id_shift', ['PUBLIC','USER'],passportCall('current'), shiftController.cancelShift); 
 
-        this.get('/:id_shift',['PUBLIC','USER'], passportCall('current'),shiftController.getShiftById); /* Consultar un turno */
+        /* Consultar un turno */
+        this.get('/:id_shift',['PUBLIC','USER'], passportCall('current'),shiftController.getShiftById); 
         this.get('/:date', ['PUBLIC','USER'], passportCall('current'),shiftController.getShiftByDate ); /* Consultar turnos por fecha */
-        this.get('/user/:id_user',['PUBLIC','USER'], passportCall('current'),shiftController.getShiftsByUser); /* Consultar turnos por usuario */
         this.post('/', ['ADMIN'], shiftController.createShift); /* Crear un turno */
         this.put('/:id_shift', ['ADMIN'], shiftController.updateShift); /* Actualizar turno */
-        this.put('/cancel/:id_shift', ['USER'], shiftController.cancelShift); /* Cancelar un turno */
         this.put('/reschedule/:id_shift', ['USER'], shiftController.rescheduleShift); /* Reprogramar un turno */
         this.delete('/:id_shift', ['ADMIN'], shiftController.deleteShift); /* Eliminar turno */
         this.put('/status/:id_shift', ['ADMIN'], shiftController.updateShiftStatus); /* Actualizar estado del turno */
